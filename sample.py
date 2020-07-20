97931798224dfef7faad28e0c630001cf2f92925
Rahul <rahulepse@gmail.com>
2020-07-20 14:38:29 +0530

    branch commit

--- sample.py
+++ sample.py
@@ -1,3 +1,11 @@
-print('Hello World')
-print('Hello Rahul')
-print('Hello')
+import datetime
+
+a = input('Please Enter Month and Year in comma seperate: ').split(',')
+
+def check_day(month,year):
+    date = datetime.date(year,month,13)
+    return 'True' if date.strftime('%A') == 'Friday' else 'False'
+
+print(check_day(int(a[0]),int(a[1])))
+
+
